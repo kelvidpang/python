886a9bd68b7ec87cbc375e0c9e8ff7468938650b
kelvidpang <kelvidpang@kelvidpangs-MacBook-Pro.local>
2017-07-22 20:53:20 +0800

    better

--- pandas_example.py
+++ pandas_example.py
@@ -8,6 +8,7 @@ Created on Sat Jul 22 20:17:16 2017
 
 import pandas as pd
 import numpy as np
+import os
 
 raw_data = {'first_name': ['Jason', 'Molly', 'Tina', 'Jake', 'Amy'],
         'last_name': ['Miller', 'Jacobson', ".", 'Milner', 'Cooze'],
@@ -15,4 +16,14 @@ raw_data = {'first_name': ['Jason', 'Molly', 'Tina', 'Jake', 'Amy'],
         'preTestScore': [4, 24, 31, ".", "."],
         'postTestScore': ["25,000", "94,000", 57, 62, 70]}
 df = pd.DataFrame(raw_data, columns = ['first_name', 'last_name', 'age', 'preTestScore', 'postTestScore'])
+df
+
+
+os.getcwd()
+os.chdir(r"/Users/kelvidpang/Desktop/python/git/python")
+
+#write to current folder
+df.to_csv('./example.csv')
+
+df = pd.read_csv('./example.csv')
 df
