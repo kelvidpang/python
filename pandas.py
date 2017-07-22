3ed768620ac8fa3e495b45892ee27a0278b41836
kelvidpang <kelvidpang@kelvidpangs-MacBook-Pro.local>
2017-07-22 20:23:08 +0800

    make it better

--- pandas.py
+++ pandas.py
@@ -8,3 +8,11 @@ Created on Sat Jul 22 20:17:16 2017
 
 import pandas as pd
 import numpy as np
+
+raw_data = {'first_name': ['Jason', 'Molly', 'Tina', 'Jake', 'Amy'],
+        'last_name': ['Miller', 'Jacobson', ".", 'Milner', 'Cooze'],
+        'age': [42, 52, 36, 24, 73],
+        'preTestScore': [4, 24, 31, ".", "."],
+        'postTestScore': ["25,000", "94,000", 57, 62, 70]}
+df = pd.DataFrame(raw_data, columns = ['first_name', 'last_name', 'age', 'preTestScore', 'postTestScore'])
+df
